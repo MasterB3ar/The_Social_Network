fa9e844f1e21f5237674450f11de493ba1913342
MasterB3ar <bailiff.talking9a@icloud.com>
2026-04-29 11:48:57 +0200

    Add script to hash secrets using bcrypt

---
+++ scripts/hash-secret.js
@@ -0,0 +1,15 @@
+const bcrypt = require('bcryptjs');
+
+const secret = process.argv.slice(2).join(' ');
+
+if (!secret) {
+  console.error('Usage: node scripts/hash-secret.js "your-password-or-layer-secret"');
+  process.exit(1);
+}
+
+bcrypt.hash(secret, 12).then((hash) => {
+  console.log(hash);
+}).catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
